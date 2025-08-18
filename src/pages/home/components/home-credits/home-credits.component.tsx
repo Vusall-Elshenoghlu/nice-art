ed74081ed0c87bb3d70936aa3fa4aa058b711aee
Vusal Huseynli <vusaleh-ap215@code.edu.az>
2025-08-18 15:54:04 +0400

    fix(home): page completed without translate

--- src/pages/home/components/home-credits/home-credits.component.tsx
+++ src/pages/home/components/home-credits/home-credits.component.tsx
@@ -1,13 +1,185 @@
 import useLocalization from '../../../../assets/lang';
 import {useHomeCreditsStyles} from './home-credits.style';
+import {useCallback, useMemo, useState} from 'react';
+import {Checkbox, Form, Input, message, Slider} from 'antd';
+import {IHomeCredits} from './home-credits';
+import {HomeCreditsModel} from './model/home-credits.model';
+import colors from '../../../../assets/styles/abstracts/color';
+import {rem} from '../../../../assets/styles/abstracts/functions';
+import { ArrowSmallRight} from '../../../../assets/images/icons/arrows';
 
 const HomeCreditsComponent = () => {
     const translate = useLocalization();
     const classes = useHomeCreditsStyles();
+    const [amount, setAmount] = useState<number>(5000);
+    const [month, setMonth] = useState<number>(12);
+    const [rate, setRate] = useState<number>(15);
+    const [form] = Form.useForm();
+
+    const monthlyRate = rate / 12 / 100;
+
+    const monthlyPayment = useMemo(() => {
+        if (!monthlyRate || !month) return 0;
+        return (amount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -month));
+    }, [amount, monthlyRate, month]);
+    console.log(monthlyPayment);
+    const initialValues = {
+        amount: 0,
+        month: 0,
+        rate: 0,
+        finCode: '',
+        contactNumber: ''
+    };
+    const onFinish = useCallback((values: IHomeCredits) => {
+        console.log('Credit values:', new HomeCreditsModel(values));
+        form.resetFields();
+    }, [form]);
+    const onFinishFailed = useCallback((errorInfo: any) => {
+        message.error('Form submission failed:', errorInfo);
+    }, [form]);
     return (
         <div className='mt-50 container'>
             <div className={classes.wrapper}>
+                <div className={'row'}>
+                    <div className={'col-lg-1'}>
+                        <div className={classes.leftDiv}>
+                            <h6>Apply to load</h6>
+                            <h1>Id duis id turpis mi
+                                quisque. Nulla.</h1>
+                            <p>Malesuada tortor fringilla ut faucibus. Urna tellus lectus platea turpis non. Tellus odio eu ante tincidunt vivamus nunc nibh arcu, augue.</p>
 
+                        </div>
+                    </div>
+                    <div className={'col-lg-11'}>
+                        <div className={classes.rightDiv}>
+                            <div className={classes.creditDiv}>
+                                <Form
+                                    initialValues={initialValues}
+                                    name={'credit'}
+                                    onFinish={onFinish}
+                                    onFinishFailed={onFinishFailed}
+
+                                >
+                                    <div className={'row'}>
+                                        <div className={'col-lg-4 col-md-4 col-sm-12'}>
+                                            <h6 className={classes.amountText}>Kreditin məbləği</h6>
+                                            <div className={classes.input}>
+                                                <h2>{amount}</h2>
+                                                <h1>AZN</h1>
+                                            </div>
+                                            <Slider
+                                                min={500}
+                                                max={50000}
+                                                step={100}
+                                                value={amount}
+                                                onChange={(val) => setAmount(val)}
+                                                className={classes.customSlider}
+                                                tooltip={{open: false}}
+                                                trackStyle={{backgroundColor: colors.activeLiColor, height: rem(2)}}
+                                                railStyle={{height: rem(2)}}
+                                                handleStyle={{width: rem(2), height: rem(2), border: 'none'}}
+
+
+                                            />
+                                        </div>
+                                        <div className={'col-lg-4 col-md-4 col-sm-12'}>
+                                            <h6 className={classes.amountText}>Kreditin müddəti</h6>
+                                            <div className={classes.input}>
+                                                <h2>{month}</h2>
+                                                <h1>Ay</h1>
+                                            </div>
+                                            <Slider
+                                                min={6}
+                                                max={120}
+                                                step={1}
+                                                value={month}
+                                                onChange={(val) => setMonth(val)}
+                                                className={classes.customSlider}
+                                                tooltip={{open: false}}
+                                                trackStyle={{backgroundColor: colors.activeLiColor, height: rem(2)}}
+                                                railStyle={{height: rem(2)}}
+                                                handleStyle={{width: rem(2), height: rem(2), border: 'none'}}
+
+
+                                            />
+                                        </div>
+                                        <div className={'col-lg-4 col-md-4 col-sm-12'}>
+                                            <h6 className={classes.amountText}>Faiz dərəcəsi</h6>
+                                            <div className={classes.input}>
+                                                <h2>{rate}</h2>
+                                                <h1>%</h1>
+                                            </div>
+                                            <Slider
+                                                min={1}
+                                                max={100}
+                                                step={1}
+                                                value={rate}
+                                                onChange={(val) => setRate(val)}
+                                                className={classes.customSlider}
+                                                tooltip={{open: false}}
+                                                trackStyle={{backgroundColor: colors.activeLiColor, height: rem(2)}}
+                                                railStyle={{height: rem(2)}}
+                                                handleStyle={{width: rem(2), height: rem(2), border: 'none'}}
+
+
+                                            />
+                                        </div>
+
+                                    </div>
+                                    <p>Aylıq ödəniş</p>
+                                    <h1>{monthlyPayment.toFixed(2)} ₼</h1>
+
+                                    <div className={classes.stringInputsDiv}>
+                                        <div className={'mt-50 row'}>
+                                            <div className={'col-lg-6 col-sm-6 col-md-12'}>
+                                                <h6>FIN code </h6>
+                                                <div className={classes.stringInput}>
+                                                    <Input
+                                                        type="text"
+                                                        bordered={false}
+                                                        placeholder='ABCDEFG'
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className={'col-lg-6 col-md-6 col-sm-12 '}>
+                                                <h6>Əlaqə nömrəsi </h6>
+                                                <div className={classes.stringInput}>
+                                                    <h5>+994</h5>
+                                                    <Input
+                                                        type="text"
+                                                        bordered={false}
+                                                        placeholder='00 000 00 00'
+                                                    />
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                    <div className={classes.checkboxAndButton}>
+                                        <div className={'row'}>
+                                            <div className={'col-lg-6 col-md-6 col-sm-12 '}>
+                                                <Checkbox>
+                                                    <p><a href="#">AKB RAZILIQ  </a> vəriləsi <br/> üçün icazə </p>
+                                                </Checkbox>
+                                            </div>
+                                            <div className={'col-lg-6 col-md-6 col-sm-12 '}>
+                                                <div className={classes.applyBtn}>
+                                                    <p>Müraciət et</p>
+                                                    <ArrowSmallRight/>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                </Form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
--- src/pages/home/components/home-credits/home-credits.style.ts
+++ src/pages/home/components/home-credits/home-credits.style.ts
@@ -1,10 +1,214 @@
 import {createUseStyles} from 'react-jss';
 import {rem} from '../../../../assets/styles/abstracts/functions';
-
+import colors from '../../../../assets/styles/abstracts/color';
+import {breakpoint} from '../../../../assets/styles/abstracts/mixins';
+import {breakpoints} from '../../../../assets/styles/abstracts/sizes';
 const styles = {
     wrapper: {
         minHeight: rem(583),
-        height: 'auto'
+        height: 'auto',
+        position: 'relative',
+
+        [breakpoint(breakpoints.tabletM)]: {
+            height: 'auto',
+            padding: rem(20),
+            backgroundColor: colors.activeLiColorWithOpacith
+        },
+        [breakpoint(breakpoints.mobileL)]: {
+            padding: rem(10),
+            backgroundColor: colors.activeLiColorWithOpacith
+
+        },
+    },
+
+    leftDiv: {
+        width: '33%',
+        height: '100%',
+        position: 'absolute',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+
+        '& h6': {
+            fontSize: rem(16),
+            fontWeight: '800',
+            lineHeight: '100%',
+            color: colors.activeLiColor,
+        },
+        '& h1': {
+            fontWeight: '800',
+            fontSize: rem(44),
+            color: colors.liColor,
+            marginTop: rem(12),
+        },
+        '& p': {
+            fontSize: rem(16),
+            fontWeight: '400',
+            lineHeight: rem(22),
+            color: colors.liColor,
+            marginTop: rem(10),
+        },
+
+        [breakpoint(breakpoints.tabletM)]: {
+            position: 'relative',
+            width: '100%',
+            marginBottom: rem(20),
+            textAlign: 'center',
+            alignItems: 'center',
+        },
+        [breakpoint(breakpoints.mobileL)]: {
+            '& h1': {
+                fontSize: rem(28),
+            },
+            '& p': {
+                fontSize: rem(14),
+                lineHeight: rem(20),
+            },
+        },
+    },
+
+    rightDiv: {
+        width: '90%',
+        marginLeft: 'auto',
+        height: rem(583),
+        backgroundColor: colors.activeLiColorWithOpacith,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingRight: rem(18),
+
+        [breakpoint(breakpoints.tabletM)]: {
+            width: '100%',
+            height: 'auto',
+            justifyContent: 'center',
+            padding: rem(20),
+            backgroundColor: 'transparent',
+        },
+        [breakpoint(breakpoints.mobileL)]: {
+            padding: rem(10),
+            backgroundColor: 'transparent',
+
+        },
+    },
+
+    creditDiv: {
+        width: rem(681),
+        minHeight: rem(343),
+        height: 'auto',
+        padding: rem(18),
+
+        '& p': {
+            color: colors.amountColor,
+            fontSize: rem(12),
+        },
+        '& h1': {
+            fontWeight: '500',
+            fontSize: rem(18),
+            color: colors.activeLiColor,
+        },
+
+        [breakpoint(breakpoints.tabletM)]: {
+            width: '100%',
+
+        },
+    },
+
+    input: {
+        width: rem(226),
+        height: rem(51),
+        backgroundColor: colors.mainBGColor,
+        border: '1px solid #F1F1F1',
+        padding: rem(20),
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: rem(10),
+
+        '& h2': {
+            fontSize: rem(16),
+        },
+        '& h1': {
+            fontSize: rem(16),
+            fontWeight: '500',
+        },
+
+        [breakpoint(breakpoints.tabletM)]: {
+            width: '100%',
+        },
+    },
+
+    stringInput: {
+        marginTop: rem(6),
+        width: rem(315),
+        height: rem(51),
+        borderRadius: rem(4),
+        backgroundColor: colors.mainBGColor,
+        border: '1px solid #F1F1F1',
+        paddingLeft: rem(15),
+        display: 'flex',
+        alignItems: 'center',
+
+        [breakpoint(breakpoints.tabletM)]: {
+            width: '100%',
+        },
+    },
+    customSlider: {
+        railBg: "red",
+        "& .ant-slider-rail": {
+            backgroundColor: 'F1F1F1',
+            height: 2,
+            marginTop: rem(-15),
+        },
+        "& .ant-slider-track": {
+            backgroundColor: colors.activeLiColor,
+            height: 2,
+            marginTop: rem(-15),
+            "&:hover": {
+                backgroundColor: colors.activeLiColor,
+            },
+        },
+        "& .ant-slider-handle": {
+
+            marginTop: rem(-15),
+            border: 'none',
+            dotBorderColor: colors.activeLiColor,
+            dotActiveBorderColor: colors.activeLiColor,
+            "&:hover": {
+                boxShadow: "none",
+
+            },
+            "&:focus": {
+                boxShadow: "none",
+            },
+        },
+
+    },
+    checkboxAndButton: {
+        marginTop: rem(15),
+    },
+
+    applyBtn: {
+        width: rem(320),
+        height: rem(48),
+        borderRadius: rem(8),
+        padding: `${rem(12)} ${rem(24)}`,
+        backgroundColor: colors.activeLiColor,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        cursor: 'pointer',
+        gap: rem(10),
+
+        '& p': {
+            color: colors.mainBGColor,
+            fontSize: rem(16),
+            lineHeight: rem(24),
+            fontWeight: '400',
+        },
+
+        [breakpoint(breakpoints.tabletM)]: {
+            width: '100%',
+        },
     },
 };
 
