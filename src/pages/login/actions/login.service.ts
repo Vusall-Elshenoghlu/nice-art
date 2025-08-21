45ea16a37cf072df959f45f88c595db763c135c7
Vusal Huseynli <vusaleh-ap215@code.edu.az>
2025-08-21 16:03:21 +0400

    fix(deleting): unnecessary folders hav been deleted

--- src/pages/login/actions/login.service.ts
+++
@@ -1,9 +0,0 @@
-import {API} from 'core/configs/api.config';
-import axiosInstance from 'core/configs/axios.config';
-import {ILoginFormValues} from '../login';
-
-export const login = async (credentials: ILoginFormValues) => {
-    const res= await axiosInstance.post(API.login, credentials);
-    return res.data;
-};
-
