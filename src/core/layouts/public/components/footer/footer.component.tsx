bdcc20457874e5f812a0c02f0bc91d311f447800
(Raooph) <(raufquliyev829@gmail.com)>
2025-08-21 12:17:06 +0400

    feat(footer): replace static labels with NavLink using Routes

--- src/core/layouts/public/components/footer/footer.component.tsx
+++ src/core/layouts/public/components/footer/footer.component.tsx
@@ -1,15 +1,19 @@
-import {environment} from 'core/configs/app.config';
-import {useFooterStyles} from './footer.style';
+import { environment } from 'core/configs/app.config';
+import { useFooterStyles } from './footer.style';
 import useLocalization from 'assets/lang';
 import logoImg from 'assets/images/statics/Logo.png';
 import {
     EmailFooterIcon,
     FacebookIcon,
     InstagramIcon,
-    LinkedinIcon, LocationFooterIcon, PhoneFooterIcon,
+    LinkedinIcon,
+    LocationFooterIcon,
+    PhoneFooterIcon,
     TwitterIcon,
     YoutubeIcon
 } from '../../../../../assets/images/icons/arrows';
+import { NavLink } from 'react-router-dom';
+import { Routes } from '../../../../../router/routes';
 
 const FooterComponent = () => {
     const classes = useFooterStyles();
@@ -17,70 +21,92 @@ const FooterComponent = () => {
     const date = new Date().getFullYear();
     const translate = useLocalization();
 
+    const socialLinks = [
+        { icon: <FacebookIcon /> },
+        { icon: <TwitterIcon /> },
+        { icon: <InstagramIcon /> },
+        { icon: <LinkedinIcon /> },
+        { icon: <YoutubeIcon /> },
+    ];
+
+    const siteLinks = [
+        { to: Routes.home, label: translate('footer_home') },
+        { to: Routes.about, label: translate('footer_about') },
+        { to: Routes.products, label: translate('footer_products') },
+        { to: Routes.partners, label: translate('footer_partners') },
+        { to: Routes.contact, label: translate('footer_contact') },
+    ];
+
+    const contactInfo = [
+        { icon: <EmailFooterIcon />, label: translate('footer_contact_company') },
+        { icon: <PhoneFooterIcon />, label: translate('footer_phone_company') },
+        { icon: <LocationFooterIcon />, label: translate('footer_location') },
+    ];
 
     return (
         <footer className={`${classes.footerSection}`}>
             <div className='container mt-120'>
-                <div className={'row'}>
-                    <div className={'col-lg-6 col-md-6 col-sm-12'}>
-                        <img src={logoImg} alt={translate('footer_logo_alt')}/>
+                <div className='row'>
+                    <div className='col-lg-6 col-md-6 col-sm-12'>
+                        <img src={logoImg} alt={translate('footer_logo_alt')} />
                         <p className={classes.footerSubtitle}>{translate('footer_subtitle')}</p>
                         <div className={classes.socialMediaIcons}>
-                            <FacebookIcon/>
-                            <TwitterIcon/>
-                            <InstagramIcon/>
-                            <LinkedinIcon/>
-                            <YoutubeIcon/>
+                            {socialLinks.map(({ icon }, index) => (
+                                <span key={index}>{icon}</span>
+                            ))}
                         </div>
                     </div>
-                    <div className={'col-lg-6 col-md-6 col-sm-12'}>
-                        <div className={'row'}>
-                            <div className={'col-6'}>
+
+                    <div className='col-lg-6 col-md-6 col-sm-12'>
+                        <div className='row'>
+                            <div className='col-6'>
                                 <div className={classes.leftDiv}>
                                     <h5 className={classes.headingLinks}>{translate('footer_site')}</h5>
-                                    <p className={`${classes.footerParagraph} mt-50`}>{translate('footer_home')}</p>
-                                    <p className={classes.footerParagraph}>{translate('footer_about')}</p>
-                                    <p className={classes.footerParagraph}>{translate('footer_products')}</p>
-                                    <p className={classes.footerParagraph}>{translate('footer_partners')}</p>
-                                    <p className={classes.footerParagraph}>{translate('footer_contact')}</p>
+                                    {siteLinks.map(({ to, label }, index) => (
+                                        <NavLink
+                                            key={to}
+                                            to={to}
+                                            className={`${classes.footerParagraph} ${index === 0 ? 'mt-50' : ''}`}
+                                        >
+                                            {label}
+                                        </NavLink>
+                                    ))}
                                 </div>
                             </div>
-                            <div className={'col-6'}>
+                            <div className='col-6'>
                                 <div className={classes.rightDiv}>
                                     <h5 className={classes.headingLinks}>{translate('footer_contact')}</h5>
-                                    <div className={`${classes.footerContactDiv} mt-50`}>
-                                        <EmailFooterIcon/>
-                                        <p>{translate('footer_contact_company')}</p>
-                                    </div>
-                                    <div className={classes.footerContactDiv}>
-                                        <PhoneFooterIcon/>
-                                        <p>{translate('footer_phone_company')}</p>
-                                    </div>
-                                    <div className={classes.footerContactDiv}>
-                                        <LocationFooterIcon/>
-                                        <p>{translate('footer_location')}</p>
-                                    </div>
+                                    {contactInfo.map(({ icon, label }, index) => (
+                                        <div
+                                            key={index}
+                                            className={`${classes.footerContactDiv} ${index === 0 ? 'mt-50' : ''}`}
+                                        >
+                                            {icon}
+                                            <p>{label}</p>
+                                        </div>
+                                    ))}
                                 </div>
-
                             </div>
-
                         </div>
                     </div>
-
                 </div>
             </div>
+
+            {/* Footer bottom */}
             <div className={classes.footerBottom}>
-                <div className={'container'}>
-                    <div className={'row'}>
-                        <div className={'col-lg-6 col-md-6 col-sm-12'}>
-                            <p className={classes.copyRight}>{translate('footer_copyright')} {date} {translate('footer_author')}</p>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-lg-6 col-md-6 col-sm-12'>
+                            <p className={classes.copyRight}>
+                                {translate('footer_copyright')} {date} {translate('footer_author')}
+                            </p>
                         </div>
-                        <div className={'col-lg-6 col-md-6 col-sm-12'}>
-                            <p className={classes.copyRight}>{translate('footer_all_rights')}  | <a href='#'>{translate('footer_terms')}</a>  | <a
-                                href="#">{translate('footer_privacy')}</a></p>
-
+                        <div className='col-lg-6 col-md-6 col-sm-12'>
+                            <p className={classes.copyRight}>
+                                {translate('footer_all_rights')} | <a href='#'>{translate('footer_terms')}</a> |{' '}
+                                <a href='#'>{translate('footer_privacy')}</a>
+                            </p>
                         </div>
-
                     </div>
                 </div>
             </div>
